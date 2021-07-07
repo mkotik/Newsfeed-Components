64e80f4706f00de63cf870f77e1b221ae2f66e00
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-07-07 16:12:57 -0400

    finished menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out",
 ];
 
 /* 
@@ -31,3 +31,35 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+// select header
+const header = document.querySelector(".header");
+
+const menuMaker = function (arr) {
+  // create elements
+  const div = document.createElement("div");
+  const ul = document.createElement("ul");
+
+  // add classes
+  div.classList.add("menu");
+
+  // assemble elements
+  div.append(ul);
+
+  // loop over array, create list items, append them into ul
+  arr.forEach((cur) => {
+    const el = document.createElement("li");
+    el.textContent = cur;
+    ul.append(el);
+  });
+
+  // select the button element, add event listener
+  const button = document.querySelector(".menu-button");
+  button.addEventListener("click", function () {
+    div.classList.toggle("menu--open");
+  });
+
+  return div;
+};
+
+header.append(menuMaker(menuItems));
